7219755d0127e0aeeb4089f1f48e68292b1c5c5e
Vedant_29_chouksey <vedantvedant119@gmail.com>
2026-09-06 14:13:01 +0530

    Change app.listen to bind to 0.0.0.0

--- backend/trackingAPI.js
+++ backend/trackingAPI.js
@@ -98,6 +98,6 @@ app.patch("/buses/:busId/status", async (req, res) => {
   }
 });
 
-app.listen(3001, () => {
+app.listen(3001, "0.0.0.0", () => {
   console.log("Tracking API running on port 3001");
 });
